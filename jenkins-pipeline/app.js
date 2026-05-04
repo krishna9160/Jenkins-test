8fbd39a2e6eb3192e0a6ce17484c11349687e1a2
root <root@ip-172-31-16-210.ap-southeast-2.compute.internal>
2026-05-04 09:32:58 +0000

    cdd

---
+++ jenkins-pipeline/app.js
@@ -0,0 +1,10 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200);
+  res.end('Hello from Jenkins Pipeline 🚀');
+});
+
+server.listen(3000, () => {
+  console.log('App running on port 3000');
+});
